037eccf05f90bded5676efc29969bb932e946862
Gutierrezflores <gutierrezflores.gf@pg.com>
2026-09-05 14:56:13 -0600

    Actualizamos el gitignore para los diferentes entornos virtuales que tenemos en varias laptops. Seguimos trabajando en el ejercicio de la tienda 3.1: 1) el ejercicio ya no se traba al momento de procesar pagos pero debemos refinar las impresiones en cada metodo, que se imprima un detalle de los datos del cliente y probar los diferentes metodos de pago que tenemos disponibles, que el programa siga trabajando con estos metodos.

--- Clases03.1.py
+++ Clases03.1.py
@@ -21,13 +21,12 @@ def main():
             self.titular = titular
 
         def procesar_pago(self):
+            print("\n\t Usted eligio: PAGO CON TARJETA")
+
             if len(self.numero_tarjeta) == 16:
-                self.monto = (- 0.02 * self.monto) + self.monto
-                print(f"\n\nPago realizado EXITOSAMENTE!!!. \nMonto total: {self.monto}")
+                self.monto = (- 0.02 * self.monto) + self.monto             
                 return True
             else:
-                print("\n\nPago Rechazado. Verifique su numero de cuenta\n")
-                print(f"{len(self.numero_tarjeta)}")
                 return False
 
     class PagoCripto(MetodoPago):
@@ -36,7 +35,7 @@ def main():
             self.__wallet_addres = wallet_addres
 
         def procesar_pago(self):
-            print(f"\n\nPago realizado EXITOSAMENTE!!!. \nMonto total: {self.monto}")
+            print("\n\t Usted eligio: PAGO CON WALLET CRIPTO")
             return True
 
 
@@ -52,10 +51,16 @@ def main():
     # Clase que gestiona los productos que el usuario elige
     class CarritoCompras:
         def __init__(self):
-            self.lista_carrito = []
+            self.lista_carrito = [] 
 
         def agregar_producto(self, producto: Producto):
             self.lista_carrito.append(producto)
+
+        def mostrar_carrito(self):
+            # primero le tenes que imprimir un ticket con los productos comprados:
+            print(f"\t Detalles de su carrito:\n")
+            for i, item in enumerate(self.lista_carrito, start=1):
+                print(f"{i} ---> {item.nombre} - (${item.precio})")
         
         def calcular_total(self):
             self.total = sum(item.precio for item in self.lista_carrito)   # Aqui dice, vamos a iterar una lista que tiene objetos, "item" representa cada objeto en esa lista, y va a entrar a cada atributo tipo "precio" generando una nueva lista con esos atributos, al final solo suma esos datos de la lista.
@@ -63,23 +68,36 @@ def main():
 
     # Clase que relaciona el carrito finalizado con un metodo de pago existente
     class orden:
-        def __init__(self, carrito: CarritoCompras, estado: str):
+        def __init__(self, carrito: CarritoCompras):
             self.carrito = carrito
-            self.estado = estado
-
-        # Obtiene el total desde el carrito, ejecuta "metodo_pago.procesar_pago()", actualiza el estado segun el resultado e imprime un recibo
-        def confirmar_orden(self, forma_pago: MetodoPago):
-            # primero le tenes que imprimir un ticket con los productos comprados:
-            print(f"\n\n ------------ Su carrito es el siguiente: ------------\n")
-            i = 0
-            for item in self.carrito.lista_carrito():
-                print(f"{i+1}) --> {item}")
+            self.estado = False
             
 
-            self.forma_pago = forma_pago
+        # Obtiene el total desde el carrito, ejecuta "metodo_pago.procesar_pago()", actualiza el estado segun el resultado e imprime un recibo
+        def confirmar_orden(self, metodo_pago: MetodoPago):
+            # Vamos a veridicar si los condicionales de los metodos de pago se efecutan de manera adecuada, actualizar el valor de estado
+
+            self.carrito.mostrar_carrito()
+
+            pago = metodo_pago
+            pago.procesar_pago()
+
+            if metodo_pago == True:
+                self.estado = True
+                print(f"\nPago realizado EXITOSAMENTE!!!. \nMonto total: {self.carrito.calcular_total()}\n")
+            else:
+                print("\n\t\tAlgo salio mal, pago rechazado. Verifique su numero de cuenta:\n")
+                print(f"\t{pago.numero_tarjeta}")
+                print(f"\t{pago.titular}")
 
 
-    print(f"\n\n -------------- Le comparto su orden de compra: --------------\n")
+
+            
+
+
+
+
+    print(f"\n\n ------------------- SU ORDEN DE COMPRA ES LA SIGUIENTE: -------------------\n")
 
 
     it1 = Producto("Coca Cola", 20)
@@ -93,18 +111,14 @@ def main():
     carritoShein.agregar_producto(it3)
     carritoShein.agregar_producto(it4)
 
-    # Si imprime total = 145 y el tipo de valor: int
-    #print(carritoShein.calcular_total())
-    #rint(type(carritoShein.calcular_total()))
+    pago_tar = PagoTarjeta(carritoShein.calcular_total(), "123456789abcdefg", "Juan Pezcador")
+    pago_wall = PagoCripto(carritoShein.calcular_total(), "Mi_Wallet_2312")
     
-    pagar_cuenta = MetodoPago(carritoShein.calcular_total())
-    pago_con_tarjeta = PagoTarjeta(pagar_cuenta, "abcd1234efgh5678", "Juan Pescador")
-
+    pagar = orden(carritoShein)
+    pagar.confirmar_orden(pago_tar)
+    
     
 
-    se_pago = pago_con_tarjeta.procesar_pago()
-
-    cuenta1 = orden(carritoShein, se_pago)
 
 
 
